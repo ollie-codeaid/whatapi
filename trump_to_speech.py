1c656f2db37098efe4dc9259523b8731f7e1355a
Oliver Lang <Oliver.Lang@growthstreet.co.uk>
2019-08-27 11:51:03 +0100

    Add some comments

--- trump_to_speech.py
+++ trump_to_speech.py
@@ -10,13 +10,18 @@ API_KEY = getenv("VOICE_RSS_API_KEY")
 TTS_BASE_URL = f"http://api.voicerss.org/?key={API_KEY}&hl=en-us&src="
 TRUMP_URL = "https://api.tronalddump.io/random/quote"
 
+
+# GET TRUMP QUOTE
 trump_response = requests.get(TRUMP_URL)
 trump_quote = trump_response.json()["value"]
 
+# ENCODE QUOTE TO SEND TO VOICE RSS
 encoded_quote = quote(trump_quote)
 
+# GET AUDIO
 text_response = requests.get(TTS_BASE_URL + encoded_quote)
 
+# WRITE AUDIO TO FILE
 file_name = f"wav_output/{uuid4()}.wav"
 with open(file_name, "bx") as f:
   f.write(text_response.content)
